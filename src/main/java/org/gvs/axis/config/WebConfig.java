8f0e4d3aca88e6b990bf60b34cc9c2cb888f7187
vitormtz <vitor.brak@hotmail.com>
2024-11-21 14:48:29 -0300

    Adjusted the configuration to redirect all not found URLs to the home page

--- src/main/java/org/gvs/axis/config/WebConfig.java
+++ src/main/java/org/gvs/axis/config/WebConfig.java
@@ -1,6 +1,11 @@
 package org.gvs.axis.config;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +23,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/");
+            container.addErrorPages(error404Page);
+        };
     }
 }
